4ccd5935f09765d9061266eb9a928b86be29a315
Sean Dick <sean@change.org>
2015-05-11 12:13:19 -0700

    first commit

---
+++ fluent-plugin-geoblipper.gemspec
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+Gem::Specification.new do |gem|
+  gem.name        = "fluent-plugin-eventcounter"
+  gem.version     = "0.0.4.1"
+  gem.authors     = ["Sean Dick", "Change.org"]
+  gem.email       = ["sean@change.org"]
+  gem.homepage    = "https://github.com/seanmdick/fluent-plugin-geoblipper"
+  gem.summary     = %q{Fluentd plugin to send a buffered list of lat/long pairs to pubnub}
+  gem.description = %q{Fluentd plugin to convert ips to latitude/longitude pairs for publication on a specified pubnub channel}
+  gem.license     = "MIT"
+
+  gem.files         = `git ls-files`.split("\n")
+  gem.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  gem.require_paths = ["lib"]
+
+  gem.add_runtime_dependency "fluentd"
+  gem.add_runtime_dependency "pubnub"
+  gem.add_runtime_dependency "geoip"
+
+  gem.add_development_dependency "bundler"
+  gem.add_development_dependency "rake"
+  gem.add_development_dependency "rspec"
+end
---
+++ lib/fluent/plugin/out_geoblipper.rb
@@ -0,0 +1,35 @@
+class Fluent::GeoBlipperOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output('geoblipper', self)
+
+  def initialize
+    super
+    require 'geoip'
+    require 'pubnub'
+  end
+
+  config_param :pubnub_channel, :string
+  config_param :pubnub_publish_key, :string
+  config_param :geodata_location, :string
+  config_param :max_entries, :integer, :default => -1
+  config_param :ip_key, :string, :default => 'ip'
+
+  def start
+    super
+    @geodata = GeoIP.new(@geodata_location)
+    @pubnub = Pubnub.new( publish_key: @pubnub_publish_key )
+  end
+
+  def format(tag, time, record)
+    address = record[@ip_key]
+    loc = @geodata.city(address)
+    {latitude: loc.latitude, longitude: loc.longitude}.to_json + "\n"
+  end
+
+  def write(chunk)
+    chunk.open do |io|
+      items = chunk.split("\n")
+      entries = items.slice(0..@max_entries).map {|item| JSON.parse(item) }
+      @pubnub.publish(message: entries, channel: @pubnub_channel)
+    end
+  end
+end
