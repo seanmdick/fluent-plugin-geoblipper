41fc82d1dd9d07dfd006d745269fc1a7d0ee193d
Sean Dick <sean@change.org>
2015-05-13 09:20:01 -0700

    use extra info if available, post only when data exists

--- lib/fluent/plugin/out_geoblipper.rb
+++ lib/fluent/plugin/out_geoblipper.rb
@@ -13,6 +13,7 @@ class Fluent::GeoBlipperOutput < Fluent::BufferedOutput
   config_param :geodata_location, :string
   config_param :max_entries, :integer, :default => -1
   config_param :ip_key, :string, :default => 'ip'
+  config_param :extra_capture, :array, :default = []
 
   def start
     super
@@ -23,8 +24,12 @@ class Fluent::GeoBlipperOutput < Fluent::BufferedOutput
   def format(tag, time, record)
     address = record[@ip_key]
     loc = @geodata.city(address)
+    extra = {}
+    extra_capture.each do |key|
+      extra[key] = record[key]
+    end
     if loc
-      {latitude: loc.latitude, longitude: loc.longitude}.to_json + "\n"
+      {latitude: loc.latitude, longitude: loc.longitude}.merge(extra).to_json + "\n"
     else
       ""
     end
@@ -34,7 +39,9 @@ class Fluent::GeoBlipperOutput < Fluent::BufferedOutput
     chunk.open do |io|
       items = io.read.split("\n")
       entries = items.slice(0..@max_entries).map {|item| JSON.parse(item) }
-      @pubnub.publish(http_sync: true, message: entries, channel: @pubnub_channel)
+      unless entries.empty?
+        @pubnub.publish(http_sync: true, message: entries, channel: @pubnub_channel)
+      end
     end
   end
 end
